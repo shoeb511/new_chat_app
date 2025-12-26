dbf0bf33e815e5dd81bba8c5f7ed0893614089c2
dean-git <shoebqureshi43@gmail.com>
2025-12-26 13:31:45 +0530

    remove node modules and implemented mongodb atlas

--- Server/server.js
+++ Server/server.js
@@ -1,48 +1,36 @@
-const http = require("http");
-const { Server } = require("socket.io");
-const app = require("./app");
+require("dotenv").config();
+const connectDB = require("./configs/db");
 
-const PORT = 3000;
+const startServer = async () => {
+    await connectDB();
 
-const server = http.createServer(app);
+    const http = require("http");
+    const { Server } = require("socket.io");
+    const app = require("./app");
 
-const io = new Server(server);
+    //const PORT = 3000;
+    const server = http.createServer(app);
+    const io = new Server(server);
 
-const text = "Hello from server";
-const id = "123abc";
-const usernamefromsocket = "annonymous";
+    io.on("connection", (socket) => {
+        socket.username = `User-${socket.id.slice(-4)}`;
 
-io.on("connection", (socket) => {
-  // CORRECTED: give every socket a default username so messages never show as anonymous
-  socket.username = `User-${socket.id.slice(-4)}`;
-  console.log("user connected:", socket.id, "as", socket.username);
+        socket.on("set_username", (username) => {
+            socket.username = username;
+        });
 
-  socket.on("set_username", (username) => {
-    // CORRECTED: update socket username when client provides one
-    socket.username = username;
-    console.log(`user ${socket.id} set username to ${username}`);
-  });
+        socket.on("chat_message", (msg) => {
+            io.emit("chat_message", {
+                text: msg,
+                id: socket.id,
+                username: socket.username
+            });
+        });
+    });
 
-  socket.on("chat_message", (msg) => {
-    console.log("message reciewved", msg);
+    server.listen(process.env.PORT, () => {
+        console.log(`Server running on port ${process.env.PORT}`);
+    });
+};
 
-    const messageData = {
-      text: msg,
-      id: socket.id,
-      username: socket.username // CORRECTED: always include username sent to clients
-    };
-    console.log("username attached to message:", socket.username);
-
-    io.emit("chat_message", messageData);
-  });
-
-  
-
-  socket.on("disconnect", () => {
-    console.log("user disconnected:", socket.id);
-  });
-});
-
-server.listen(PORT, () => {
-  console.log("Server running on port 3000");
-});
+startServer();
